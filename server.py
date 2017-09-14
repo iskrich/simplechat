43cba7ce66ecf5f94fb4abc60a487009d5e9b5c0
Dmitry Iskrich <iskrich@inbox.ru>
2017-09-14 22:45:45 +0300

    Server side: use select for collecting sockets, store nicknames by sockets

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import errno
 import datetime
+import select
+
 #maybe need extend
 buffer_size = 1024
 port = 1111
@@ -10,45 +12,45 @@ class SimpleChatServer:
         #start server socket
         print("Starting server on localhost:%d" % port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setblocking(0)
         self.socket.bind(('localhost', port))
         self.socket.listen(100)
         self.currentUsers = {}
+        self.currentUsers[self.socket] = "Server"
 
     def _processNewUsers(self):
         connect, address = self.socket.accept()
-        msg = connect.recv(buffer_size)
-
-        if address not in self.currentUsers:
-            self.currentUsers[address] = {"nickname": msg, "socket": connect}
-            connect.send("Welcome to chat %s, type 'exit' for leaving from chat\n" % msg)
-            self._broadcast(msg, "Connected to chat")
+        nick = connect.recv(buffer_size)
+        self.currentUsers[connect] = nick
+        connect.send("Welcome to chat %s, type 'exit' for leaving from chat\n" % nick)
+        self._broadcast(connect, "Connected to chat")
 
     def _listenUsers(self):
-        for user in self.currentUsers.itervalues():
-            s = user["socket"]
-            data = s.recv(buffer_size)
-            if data:
-                self._broadcast(user["nickname"], data)
+        reads, writes, execs = select.select(self.currentUsers.keys(), [], [])
 
-    def _broadcast(self, nick, msg):
+        for s in reads:
+            if s is self.socket:
+                self._processNewUsers()
+            else:
+                msg = s.recv(buffer_size)
+                if msg:
+                    self._broadcast(s, msg)
+
+    def _broadcast(self, sock, msg):
         now = datetime.datetime.now()
         msg = "[%d:%d:%d] %s: %s" % (now.hour, now.minute, now.second,
-                                     nick, msg)
-        for adr in self.currentUsers.keys():
+                                     self.currentUsers[sock], msg)
+        for s in self.currentUsers.keys():
             try:
-                self.currentUsers[adr]["socket"].send(msg + "\n")
+                if s != self.socket and s != sock:
+                    s.send(msg + "\n")
             except socket.error as er:
                 if er.errno == errno.WSAECONNRESET:
-                    self.currentUsers[adr].close()
-                    del self.currentUsers[adr]
+                    del self.currentUsers[s]
         print(msg)
 
     def start(self):
         while True:
             self._listenUsers()
-            self._processNewUsers()
-
 
 
 if __name__ == "__main__":
