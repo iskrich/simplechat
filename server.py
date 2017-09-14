52a9714031917a9b4686183c035f6249f090b3a8
Dmitry Iskrich <iskrich@inbox.ru>
2017-09-14 20:28:08 +0300

    Server side: divide main loop into Process new users and Listen users

--- server.py
+++ server.py
@@ -13,29 +13,41 @@ class SimpleChatServer:
         self.socket.bind(('localhost', port))
         self.socket.listen(100)
         self.currentUsers = {}
-        
+
+    def _processNewUsers(self):
+        connect, address = self.socket.accept()
+        msg = connect.recv(buffer_size)
+
+        if address not in self.currentUsers:
+            self.currentUsers[address] = {"nickname": msg, "socket": connect}
+            connect.send("Welcome to chat %s, type 'exit' for leaving from chat\n" % msg)
+            self._broadcast(msg, "Connected to chat")
+
+    def _listenUsers(self):
+        for user in self.currentUsers.itervalues():
+            s = user["socket"]
+            data = s.recv(buffer_size)
+            if data:
+                self._broadcast(user["nickname"], data)
+
+    def _broadcast(self, nick, msg):
+        now = datetime.datetime.now()
+        msg = "[%d:%d:%d] %s: %s" % (now.hour, now.minute, now.second,
+                                     nick, msg)
+        for adr in self.currentUsers.keys():
+            try:
+                self.currentUsers[adr]["socket"].send(msg + "\n")
+            except socket.error as er:
+                if er.errno == errno.WSAECONNRESET:
+                    del self.currentUsers[adr]
+        print(msg)
+
     def start(self):
         while True:
+            self._listenUsers()
+            self._processNewUsers()
 
-            connect, address = self.socket.accept()
-            msg = connect.recv(buffer_size)
 
-            if address not in self.currentUsers:
-                self.currentUsers[address] = {"nickname": msg, "socket": connect}
-                connect.send("Welcome to chat %s, type 'exit' for leaving from chat\n" % msg)
-                msg = "%s connected to chat\n" % msg
-            else:
-                now = datetime.datetime.now()
-                msg = "[%d:%d:%d] %s: %s" % (now.hour, now.minute, now.second,
-                                             self.currentUsers[address]["nickname"], msg)
-
-            for adr in self.currentUsers.keys():
-                try:
-                    self.currentUsers[adr]["socket"].send(msg + "\n")
-                except socket.error as er:
-                    if er.errno == errno.WSAECONNRESET:
-                        del self.currentUsers[adr]
-            print(msg)
 
 if __name__ == "__main__":
     server = SimpleChatServer()
